ed38251ab8b9bea5813e0e4514d6661a68ed10a2
ohnewdev <ohnewdev@gmail.com>
2021-04-11 14:54:27 +0900

    Add Facoties

--- AbstFactoryPattern2/Program.cs
+++ AbstFactoryPattern2/Program.cs
@@ -1,30 +1,49 @@
 ﻿using System;
 
-// 추상팩토리 
-// 샘플2
-// ref : https://www.youtube.com/watch?v=soV1C6j9kkg&list=PLsoscMhnRc7pPsRHmgN4M8tqUdWZzkpxY&index=11
+/// <summary>
+/// 
+///
+/// 추상팩토리 
+/// 샘플2
+/// 
+/// 요구사항
+/// 
+///
+/// ref : https://www.youtube.com/watch?v=soV1C6j9kkg&list=PLsoscMhnRc7pPsRHmgN4M8tqUdWZzkpxY&index=11
+/// </summary>
 namespace AbstFactoryPattern2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            IGuiFac fac = new LinuxGuiFac();
+
+
+
+            //Console.WriteLine("Hello World!");
+            //IGuiFac fac = new LinuxGuiFac();
+            //IButton btn = fac.createButton();
+            //btn.click();
+
+
+            //ITextarea ta = fac.createTextarea();
+            //Console.WriteLine(ta.getText());
+
+
+            //// Mac
+            //fac = new MacGuiFac();
+            //btn = fac.createButton();
+            //btn.click();
+            //ta = fac.createTextarea();
+            //Console.WriteLine(ta.getText());
+
+
+            IGuiFac fac = FactoryInstance.getGuiFac();
             IButton btn = fac.createButton();
             btn.click();
-
-
             ITextarea ta = fac.createTextarea();
             Console.WriteLine(ta.getText());
 
-
-            // Mac
-            fac = new MacFac();
-            btn = fac.createButton();
-            btn.click();
-            ta = fac.createTextarea();
-            Console.WriteLine(ta.getText());
 
         }
     }
@@ -58,6 +77,8 @@ namespace AbstFactoryPattern2
         }
     }
 
+
+
     public class LinuxButton : IButton
     {
         public void click()
@@ -75,7 +96,7 @@ namespace AbstFactoryPattern2
     }
 
     // add new factory
-    public class MacFac : IGuiFac
+    public class MacGuiFac : IGuiFac
     {
         public IButton createButton()
         {
@@ -103,5 +124,82 @@ namespace AbstFactoryPattern2
             Console.WriteLine("Mac button 클릭");
         }
 
+    }
+
+    // add new fac
+    public class WindowGuiFac : IGuiFac
+    {
+        public IButton createButton()
+        {
+            return new WindowButton();
+        }
+
+        public ITextarea createTextarea()
+        {
+            return new WindowTextarea();
+        }
+    }
+
+
+
+    public class WindowButton : IButton
+    {
+        public void click()
+        {
+            Console.WriteLine("Window button click!!");
+        }
+    }
+
+    public class WindowTextarea : ITextarea
+    {
+        public string getText()
+        {
+            return "Window string on Textarea";
+        }
+    }
+
+
+    public class FactoryInstance
+    {
+        public static IGuiFac getGuiFac()
+        {
+            int l_typeOs = getTypeOfOs();
+            switch (l_typeOs)
+            {
+                case 0:
+                    return new WindowGuiFac();
+                case 1:
+                    return new MacGuiFac();
+                case 2:
+                    return new LinuxGuiFac();
+
+                default:
+                    return null;
+
+            }
+
+        }
+
+        private static int getTypeOfOs()
+        {
+
+            // Microsoft Windows
+            Console.WriteLine(Environment.OSVersion.ToString());
+            if (Environment.OSVersion.ToString().Contains("Windows"))
+            {
+                return 0;
+
+            }
+            else if (Environment.OSVersion.ToString().Contains("Mac"))
+            {
+                return 1;
+            }
+            else if (Environment.OSVersion.ToString().Contains("Linux"))
+            {
+                return 2;
+            }
+            else return -1;
+
+        }
     }
 }
