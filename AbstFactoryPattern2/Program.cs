d6f7b0694b0c29f95d738ae932031e7c03716147
ohnewdev <ohnewdev@gmail.com>
2021-04-11 14:33:53 +0900

    Add project files.

---
+++ AbstFactoryPattern2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace AbstFactoryPattern2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IGuiFac fac = new LinuxGuiFac();
+            IButton btn = fac.createButton();
+            btn.click();
+
+
+            ITextarea ta = fac.createTextarea();
+            Console.WriteLine(ta.getText());
+
+
+            // Mac
+            fac = new MacFac();
+            btn = fac.createButton();
+            btn.click();
+            ta = fac.createTextarea();
+            Console.WriteLine(ta.getText());
+
+        }
+    }
+
+    public interface IGuiFac
+    {
+        public IButton createButton();
+        public ITextarea createTextarea();
+    }
+
+    public interface IButton
+    {
+        public void click();
+    }
+
+    public interface ITextarea
+    {
+        public string getText();
+    }
+
+    public class LinuxGuiFac : IGuiFac
+    {
+        public IButton createButton()
+        {
+            return new LinuxButton();
+        }
+
+        public ITextarea createTextarea()
+        {
+            return new LinuxTextarea();
+        }
+    }
+
+    public class LinuxButton : IButton
+    {
+        public void click()
+        {
+            Console.WriteLine("Linux button click!!");
+        }
+    }
+
+    public class LinuxTextarea : ITextarea
+    {
+        public string getText()
+        {
+            return "Linux string on Textarea";
+        }
+    }
+
+    // add new factory
+    public class MacFac : IGuiFac
+    {
+        public IButton createButton()
+        {
+            return new MacButton();
+        }
+
+        public ITextarea createTextarea()
+        {
+            return new MacTextArea();
+        }
+    }
+
+    internal class MacTextArea : ITextarea
+    {
+        public string getText()
+        {
+            return "Mac's string on TextArea";
+        }
+    }
+
+    internal class MacButton : IButton
+    {
+        public void click()
+        {
+            Console.WriteLine("Mac button 클릭");
+        }
+
+    }
+}
